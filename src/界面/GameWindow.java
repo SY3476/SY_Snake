9c0ce68071a1273085ba5397153a5fa39e42ced6
SY34 <SY34@bucunzaideyouxiang.com>
2026-03-02 21:03:35 +0800

    核心逻辑及画面渲染大部

--- src/界面/GameWindow.java
+++ src/界面/GameWindow.java
@@ -1,7 +1,5 @@
 package 界面;
 
-import org.w3c.dom.ls.LSOutput;
-
 import javax.swing.*;
 
 import java.awt.*;
@@ -36,14 +34,39 @@ public class GameWindow extends JFrame implements KeyListener, ActionListener {
         snake.add(new Point(10,15));
         snake.add(new Point(10,14));
 
-        direction="RIGHT";
+        direction="R";
 
         isGameOver=false;
 
         timer = new Timer(Speed,this);
         timer.start();
 
+
     }
+    public void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new GameWindow());
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        if (isGameOver) {
+            g.setColor(Color.red);
+            g.
+        }//进行游戏结束处理，暂时搁置
+        g.setColor(Color.yellow);
+        Point head = snake.getFirst();
+        g.fillRect(head.x*Blook_Size,head.y*Blook_Size,Blook_Size,Blook_Size);
+
+        g.setColor(Color.white);
+        snake.stream()
+                .skip(1)
+                .forEach((e)->{g.fillRect(e.x*Blook_Size,e.y*Blook_Size,Blook_Size,Blook_Size);});
+
+        g.setColor(Color.red);
+        g.fillRect(food.x*Blook_Size,food.y*Blook_Size,Blook_Size,Blook_Size);
+    }
+
     private void  generateFood(){
         Random rand = new Random();
         do{
@@ -51,14 +74,42 @@ public class GameWindow extends JFrame implements KeyListener, ActionListener {
         }while(snake.contains(food));
     }
 
-    public void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new GameWindow());
-    }
-
     @Override
     public void actionPerformed(ActionEvent e) {
+        if(isGameOver){
+            return;
+        }//结束判断
 
-    }
+        Point newhead = snake.getFirst();
+        if (direction.equals("R")) {
+            newhead.x=snake.getFirst().x+1;
+            newhead.y=snake.getFirst().y;
+        }else  if (direction.equals("L")) {
+            newhead.x=snake.getFirst().x-1;
+            newhead.y=snake.getFirst().y;
+        }else  if (direction.equals("U")) {
+            newhead.x=snake.getFirst().x;
+            newhead.y=snake.getFirst().y-1;
+        }else  if (direction.equals("D")) {
+            newhead.x=snake.getFirst().x;
+            newhead.y=snake.getFirst().y+1;
+        }//运动
+
+        if((newhead.x==0 || newhead.y==0 || newhead.y==31 ||newhead.x==21)||snake.contains(newhead)){
+            isGameOver=true;
+            timer.stop();
+            return;
+        }//碰撞检测
+
+        snake.add(0,newhead);
+        if(newhead.equals(food)) {
+            generateFood();
+        }else {
+            snake.remove(snake.size() - 1);
+        }//干饭检测
+
+        repaint();
+}
 
     @Override
     public void keyTyped(KeyEvent e) {
